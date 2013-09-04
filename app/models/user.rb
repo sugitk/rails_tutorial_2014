2152f58a11865cbd4cec20f50502affa4cfe027c
SUGIMURA Takashi <t@sugimura.cc>
2013-09-04 17:57:51 +0900

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,20 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
 
   has_secure_password
+
+  before_create :create_remember_token
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
